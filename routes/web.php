1b1e86d7a018671a4f001d78f027b9a4a48b12d6
Faisal <faisalvin15@gmail.com>
2022-03-11 23:34:22 +0700

    Create migrations from erd

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
 Route::resource('/', LandingController::class);
 Route::get('/clubs', [LandingController::class, 'clubs'])->name('pages.clubs');
